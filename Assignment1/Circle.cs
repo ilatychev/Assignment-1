b44666e44ef606f0d6b97468055134c4fc825807
Igor Latychev <you@example.com>
2020-01-24 16:46:03 -0500

    Containing the completed circle class

---
+++ Assignment1/Circle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment1
+{
+    public class Circle
+    {
+        private int radius;
+
+        public Circle()
+        {
+            radius = 1;
+        }
+
+        public Circle(int newRadius)
+        {
+            radius = newRadius;
+        }
+
+        public int GetRadius()
+        {
+            return radius;
+        }
+
+        public void SetRadius(int newRadius)
+        {
+            radius = newRadius;
+        }
+
+        public double GetCircumference()
+        {
+            return 2 * 3.14 * radius;
+        }
+
+        public double GetArea()
+        {
+            return 3.14 * radius * 2;
+        } 
+    }
+}
